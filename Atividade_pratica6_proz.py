140c1e06e19d2f7d47c5312a0456f1d29137ffb4
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:21:03 -0300

    Add files via upload

---
+++ Atividade_pratica6_proz.py
@@ -0,0 +1,10 @@
+import math
+
+# Solicita o raio do círculo ao usuário
+raio = float(input("Digite o raio do círculo: "))
+
+# Calcula a área usando a fórmula
+area = math.pi * raio ** 2
+
+# Exibe o resultado
+print(f"A área do círculo com raio {raio} é {area:.2f}")
