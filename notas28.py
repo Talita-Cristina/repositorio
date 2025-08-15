608fcc23078ad93a3f7e716f93db6eb7fe6370b2
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:20:08 -0300

    Add files via upload

---
+++ notas28.py
@@ -0,0 +1,17 @@
+# Programa para verificar se o aluno passou.
+
+nota = float (input("DIGITE A NOTA (0 a 10): "))
+
+while nota <0 and nota > 10:
+
+    print ("Erro! A NOTA DE SER ENTRE 0 A 10")
+
+    nota = float (input ("DIGITE A NOTA NOVAMENTE: "))
+
+if nota >= 7:
+
+ print ("ALUNO APROVADO!")
+
+else:
+   
+   print ("ALUNO REPROVADO!")
