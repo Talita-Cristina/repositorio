3ad5e013e7843d2f093dbd7433dff7b2ca763ba5
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:16:54 -0300

    Add files via upload

---
+++ olamundo29.py
@@ -0,0 +1,28 @@
+print ("DIGITE O NUMERO DO SEU CARTÃO")
+cartão = input()
+
+print ("DIGITE SEU NOME")
+nome = input ()
+
+print ("DIGITE A SUA IDADE:")
+idade = input()
+
+print ("DIGITE SEU SEXO (M/F)")
+sexo = input()
+
+print ("DIGITE O NOME DE SUA RUA /N°:")
+rua = input()
+
+print ("DIGITE O SEU BAIRRO")
+bairro = input()
+
+print ("DIGITE A SUA CIDADE")
+cidade = input()
+
+print ("----------------------------------------------")
+print ("O NUMERO DO SEU CARTÃO É: " , cartão)
+print ("O SEU NOME É: " , nome)
+print ("A SUA IDADE É: " , idade)
+print ("O SEU SEXO É: " , sexo)
+print ("O SEU ENDEREÇO É : RUA : " , rua, "BAIRRO :", bairro, "CIDADE : ", cidade)
+print (f"O SEU ENDEREÇO É -> RUA :  {rua} - BAIRRO : {bairro} CIDADE : {cidade}")
