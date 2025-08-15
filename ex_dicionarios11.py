ece64a80723adc6ebfd72d45c0ba71d5f56fca03
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:21:48 -0300

    Add files via upload

---
+++ ex_dicionarios11.py
@@ -0,0 +1,14 @@
+Daniela = {
+            'nome': 'Daniela',
+            'sobrenome': 'Souza',
+            'profissao': 'Analista de Sistemas',
+            'filhos': 'Helena'
+          }
+
+print(Daniela['nome'])
+print(Daniela['sobrenome'])
+print(Daniela['profissao'])
+print(Daniela['filhos'])
+print() # Quebra de linha na saída
+# Para verificar a quantidade de elemenetos do dicionário
+print (len(Daniela))
