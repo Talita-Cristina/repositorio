11fd32ab2e73e1db6c4028f20ccc819accc2dc96
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:18:53 -0300

    Add files via upload

---
+++ ex1procedimento13.py
@@ -0,0 +1,14 @@
+# Algoritmo "Exemplo1_Procedimento"
+
+# Criando um procedimento ( Em Python, definimos 
+# funções para procedimento)
+def saudacao ():
+    #inicio
+    print("Olá mundo!")
+    # fimprocedimento
+
+# var
+
+ # Chamado um procedimento (em Python, chamanos
+ # a função definida)
+    saudacao()
