7a4205c4badd82eb7a0a70ade4298936c111841a
Talita-Cristina <cristinatalita49@gmail.com>
2025-08-15 19:27:42 -0300

    Add files via upload

---
+++ vetor.py
@@ -0,0 +1,10 @@
+# Em Python, não precisamos declarar o tipo r tamanho fixo
+# do vetor antecipadamente.
+
+# Cria uma lista com 4 elementos, inicializados com 0
+vetorDeInteiros = [0] * 4 #vetor que vai do índice 0 até 4
+
+vetorDeInteiros[0] = 2
+
+# Usamos f-strings para formatar a saída de forma concisa.
+print(f"Na posição 0, o valor é.: {vetorDeInteiros[0]}")
